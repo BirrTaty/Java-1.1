39222506eee0c33276bfd14cc430f7ba0c155379
Tatiana <birrtaty@gmail.com>
2021-10-02 18:56:36 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    int accountBalance = 2000000000;
-    int transferAmount = 500000000;
-    int totalSum = accountBalance + transferAmount;
-    System.out.println(totalSum);
+        int accountBalance = 2000000000;
+        int transferAmount = 500000000;
+        int totalSum = accountBalance + transferAmount;
+        System.out.println(totalSum);
     }
 }
